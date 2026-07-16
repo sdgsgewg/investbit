aadc6330bb313440b54db9fd193d820fe7fd3ec5
Jessen <jessen123ptk@gmail.com>
2026-07-16 17:04:37 +0700

    change supabase key

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: Request) {
     const cookieStore = await cookies();
     const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
     const supabaseKey =
-      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
     if (supabaseUrl && supabaseKey) {
       const supabase = createServerClient(supabaseUrl, supabaseKey, {
